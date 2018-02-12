3982fc2c2698c1a31150b27603f3b0ee8ff741fa
Merrill Lines <linesmerrill@gmail.com>
2018-02-12 14:09:25 -0700

    fix cors problem locally

--- api/routes/projectsListRoutes.js
+++ api/routes/projectsListRoutes.js
@@ -3,6 +3,13 @@
 module.exports = function (app) {
     var projectList = require('../controllers/projectsListControllers')
 
+    /* Setting the headers allows for CORS so it will run normal locally */
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.route('/projects')
         .get(projectList.list_all_projects)
         .post(projectList.create_a_project);
