102e04637ceac64e76b810bd985af307274d91ee
Merrill Lines <linesmerrill@gmail.com>
2018-02-14 18:47:53 -0700

    Filter projects being displayed by specific user

--- api/models/projectsListModels.js
+++ api/models/projectsListModels.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
+    username: {
+        type: String,
+    },
     name: {
         type: String,
         Required: 'Please enter'
