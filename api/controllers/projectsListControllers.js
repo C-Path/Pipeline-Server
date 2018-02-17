a48030dde712a23b822a95d6ae770856afbf5867
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-02-16 18:59:51 -0700

    saving project on new data object

--- api/controllers/projectsListControllers.js
+++ api/controllers/projectsListControllers.js
@@ -4,16 +4,16 @@ var mongoose = require('mongoose'),
     Project = mongoose.model('Projects');
 
 exports.list_all_projects = function (req, res) {
-    Project.find({"username":req.query.username}, function (err, project) {
+    Project.find({"username":req.query.username}, function (err, projects) {
         if (err)
             res.send(err);
-        res.json(project);
+        res.json(projects);
     });
 };
 
 exports.create_a_project = function (req, res) {
-    console.log("HEEEEY", req.body.project)
-    var new_project = new Project(req.body.project);
+    console.log("HEEEEY", req.body)
+    var new_project = new Project(req.body);
     console.log("Received: ", new_project)
     new_project.save(function (err, project) {
         if (err)
